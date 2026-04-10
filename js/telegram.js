e3043e1efd48cdc80692301cedee59409abf01a0
DavidSaatchyan <saatchy.d@gmail.com>
2026-04-10 16:19:10 +0800

    Add files via upload

--- js/telegram.js
+++ js/telegram.js
@@ -3,6 +3,11 @@ const tg = window.Telegram.WebApp;
 // Расширение на весь экран
 tg.expand();
 
+// Сообщаем Telegram что WebApp готов
+try {
+    tg.ready();
+} catch (e) {}
+
 // Настройка главной кнопки (если нужно)
 tg.MainButton.setText('Готово');
 tg.MainButton.onClick(() => {
@@ -15,3 +20,14 @@ document.documentElement.style.setProperty('--text', tg.themeParams.text_color |
 
 // Убираем кнопку "Назад" по умолчанию, если нужно
 tg.BackButton.hide();
+
+// Убираем залипающий фокус/подсветку после тапа (особенно заметно в Telegram mobile)
+const blurActive = () => {
+    try {
+        const el = document.activeElement;
+        if (el && typeof el.blur === 'function') el.blur();
+    } catch (e) {}
+};
+
+document.addEventListener('pointerup', blurActive, { capture: true });
+document.addEventListener('touchend', blurActive, { capture: true, passive: true });
